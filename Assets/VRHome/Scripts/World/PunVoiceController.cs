5db9c43308afae919bc16553e0b61f42fcb384d7
meow.ren9 <meow.ren9@gmail.com>
2018-10-31 18:58:39 -0400

    voice distance control

---
+++ Assets/VRHome/Scripts/World/PunVoiceController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunVoiceController : MonoBehaviour {
+
+	private PhotonVoiceRecorder photonVoiceRecorder;
+
+	void Awake() {
+		photonVoiceRecorder= GetComponent<PhotonVoiceRecorder>();
+	}
+	
+	void Update () {
+
+		if(photonVoiceRecorder==null) return;
+
+		var playerList = GameObject.FindGameObjectsWithTag("Player");
+
+		if(playerList.Length >= 2) {
+			double distance = Vector3.Distance(playerList[0].transform.position,playerList[1].transform.position);
+			
+			if(distance < 10) {
+				photonVoiceRecorder.Transmit = true;
+			}else {
+				photonVoiceRecorder.Transmit = false;
+			}
+
+		}
+
+	}
+}
