e1163532fc2cf4deb37ca5a13f365a07605d2eec
meow.ren9 <meow.ren9@gmail.com>
2018-11-02 23:16:53 -0400

    Add exvitation

---
+++ Assets/VRHome/Scripts/World/ExvitationPromptController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExvitationPromptController : MonoBehaviour {
+
+
+	private AudioSource audioSource;
+
+	private PhotonView photonView;
+	
+	public AudioClip clockSound;
+
+	public GameObject rain;
+
+	void Awake () {
+		audioSource = GetComponent<AudioSource>();
+		photonView = GetComponent<PhotonView>();
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.C)) {
+			photonView.RPC("PlayClockSound", PhotonTargets.All);
+		}
+
+		if(Input.GetKeyDown(KeyCode.R)) {
+			photonView.RPC("PlayRainEffect", PhotonTargets.All);
+		}
+	}
+
+	[PunRPC]
+	public void PlayClockSound() {
+		audioSource.PlayOneShot(clockSound);
+	}
+
+	[PunRPC]
+	public void PlayRainEffect() {
+		rain.SetActive(true);
+	}
+
+
+}
