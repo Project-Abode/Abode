233d4129e2e204e48a5bc6f51bdaf6bb3b1239df
meow.ren9 <meow.ren9@gmail.com>
2018-11-09 13:14:13 -0500

    elevator button single controll

--- Assets/VRHome/Scripts/World/Elevator/ElevatorButton.cs
+++ Assets/VRHome/Scripts/World/Elevator/ElevatorButton.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using ExitGames.SportShooting;
 public class ElevatorButton : MonoBehaviour {
 
 	public delegate void NotifyTouched();
@@ -10,6 +10,16 @@ public class ElevatorButton : MonoBehaviour {
 	void OnTriggerEnter(Collider col) {
 
 		if(col.gameObject.tag != "Hand") return;
+
+		Player collidePlayer = col.gameObject.GetComponentInParent<Player>();
+		
+		if(collidePlayer == null) return;
+
+		//Only Current Player can control
+		if(!collidePlayer.playerId.Equals(Settings.instance.id)) 
+			return;
+
+		
 		
 		if(notifyTouched!=null)
 			notifyTouched();
