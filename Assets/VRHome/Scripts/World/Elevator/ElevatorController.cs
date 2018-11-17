8f1d5d89c58bf2a025c6be955b2fe5887bac5fa2
meow.ren9 <meow.ren9@gmail.com>
2018-11-17 18:49:39 -0500

    fix elevator localposition

--- Assets/VRHome/Scripts/World/Elevator/ElevatorController.cs
+++ Assets/VRHome/Scripts/World/Elevator/ElevatorController.cs
@@ -40,6 +40,8 @@ public class ElevatorController : MonoBehaviour {
 
 	Coroutine doorCoroutine;
 	float doorOPTime = 1.855f;
+	Vector3 doorClosedPosition = new Vector3(0.653879f, 1.0874f, 0.3297522f);
+	Vector3 doorOpenPosition =  new Vector3(0.653879f, 1.0874f, 1.3297522f);
 
 	[PunRPC]
 	void CloseDoorRPC() {
@@ -53,22 +55,22 @@ public class ElevatorController : MonoBehaviour {
 		}
 		
 		doorCoroutine = StartCoroutine(DoorCoroutine(-1,doorOPTime));
-;
-
 		
 	}
 
 	IEnumerator DoorCoroutine(int dir, float time){
 
 		audioSource.Play();
-		Vector3 target = doorObject.transform.position + new Vector3(0,0,dir);
-		Vector3 start = doorObject.transform.position;
+
+
+		Vector3 target = (dir == 1)? doorOpenPosition : doorClosedPosition;
+		Vector3 start = doorObject.transform.localPosition;
 		float elapsedTime = 0f;
       
  
       while (elapsedTime < time)
       {
-		doorObject.transform.position = Vector3.Lerp(start, target, (elapsedTime / time));
+		 doorObject.transform.localPosition = Vector3.Lerp(start, target, (elapsedTime / time));
           elapsedTime += Time.deltaTime;
         yield return new WaitForEndOfFrame();
       }
