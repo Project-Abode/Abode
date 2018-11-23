4dc7469ec9d9b9faab1231d711f59ddebfeccfe0
Ketul Majmudar <kmajmuda@andrew.ad.cmu.edu>
2018-11-23 13:06:53 -0500

    level streaming guest layout changes

---
+++ Assets/Candle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : MonoBehaviour {
+    public float time_passed;
+    public float speed_of_motion;
+    public float end_time;
+    public GameObject fire_particle_effect;
+	// Use this for initialization
+	void Start () {
+        time_passed = Time.time;
+        //speed = 1.0f;
+        //end = 300.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time_passed = Time.time;
+        if (Time.time  < end_time) { 
+        transform.position += speed_of_motion * Vector3.down * Time.deltaTime/200;
+        }
+        if (Time.time > end_time)
+        {
+            Destroy(fire_particle_effect);
+        }
+    }
+}
---
+++ Assets/Dieout.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dieout : MonoBehaviour {
+    public float t;
+    public float end;
+    // Use this for initialization
+    void Start () {
+        t = Time.time;
+        end = 10.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time - t > end)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
