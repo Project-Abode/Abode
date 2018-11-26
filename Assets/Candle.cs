816cfbf1c744f505146de0116fe24d25617b28b6
Ketul Majmudar <ketul1776@gmail.com>
2018-11-25 22:27:58 -0500

    added candles

--- Assets/Candle.cs
+++ Assets/Candle.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Candle : MonoBehaviour {
+public class Candle : MonoBehaviour
+{
     public float time_passed;
-    public float speed_of_motion;
-    public float end_time;
+    public float speed_of_burning;
+    //public float end_time;
     public GameObject fire_particle_effect;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         time_passed = Time.time;
         //speed = 1.0f;
         //end = 300.0f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         time_passed = Time.time;
-        if (Time.time  < end_time) { 
-        transform.position += speed_of_motion * Vector3.down * Time.deltaTime/200;
+        if (transform.localScale.y > 0)
+        {
+            transform.localScale += speed_of_burning * Vector3.down * Time.deltaTime / 200;
         }
-        if (Time.time > end_time)
+        if (transform.localScale.y < 0)
         {
             Destroy(fire_particle_effect);
         }
