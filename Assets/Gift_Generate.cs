1c85d805879c8e6f76b3f7f9f5bb3d22d7b985df
Ketul Majmudar <ketul1776@gmail.com>
2018-12-12 09:52:25 -0500

    lighting changes to level streaming

---
+++ Assets/Gift_Generate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gift_Generate : MonoBehaviour {
+    public GameObject box;
+	// Use this for initialization
+	void Start () {
+        Instantiate(box);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Surprise.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Surprise : MonoBehaviour {
+    //public GameObject surprise;
+    public GameObject g1;
+    public GameObject g2;
+    public GameObject g3;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        float r = Random.Range(0.0f, 3.0f);
+        if (r < 1)
+            Instantiate(g1); 
+        if(r>1 & r<2)
+            Instantiate(g2);
+        if(r>2)
+            Instantiate(g3);
+        Destroy(this.gameObject);
+    }
+}
